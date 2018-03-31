9d95e21a684b95bea5c3cc313e023126e1c2ce16
keitheous <keithchongwy@gmail.com>
2018-03-31 19:52:08 +1100

    hello world

---
+++ hello_world.ex
@@ -0,0 +1,13 @@
+defmodule HelloWorld do
+  def print do
+    IO.puts 'Hello World'
+  end
+end
+
+HelloWorld.print
+
+IO.puts "My name is Keith!"
+
+IO.puts "I'm learning Elixir \n - please bear with me!"
+
+IO.puts "Exciting days ahead.."
